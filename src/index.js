65f6f9843f5c6067b34509762f1b10bfc8ba98b6
jose-digiweb <josefurtado.digital@gmail.com>
2021-08-22 00:13:14 +0200

    testing some CSS issues

--- src/index.js
+++ src/index.js
@@ -10,25 +10,6 @@ import * as model from './javaScript/model';
 import QuoteView from './javaScript/views/quoteView';
 import TimeView from './javaScript/views/timeView';
 
-function hideAddressBar() {
-  if (!window.location.hash) {
-    if (document.height < window.outerHeight) {
-      document.body.style.height = window.outerHeight + 50 + 'px';
-    }
-
-    setTimeout(function () {
-      window.scrollTo(0, 1);
-    }, 50);
-  }
-}
-
-window.addEventListener('load', function () {
-  if (!window.pageYOffset) {
-    hideAddressBar();
-  }
-});
-window.addEventListener('orientationchange', hideAddressBar);
-
 const quoteControl = async () => {
   try {
     //--> Render Loading Spinner
