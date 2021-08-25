a0ae1117afeb56be084ee6af4dbadb69695f121b
jose-digiweb <josefurtado.digital@gmail.com>
2021-08-25 15:51:57 +0200

    Set the environment variables for API Key

--- src/javaScript/config.js
+++ src/javaScript/config.js
@@ -3,7 +3,7 @@ export const QUOTE_URL = 'https://api.quotable.io/random';
 export const TIME_URL = 'https://worldtimeapi.org/api/ip';
 
 export const IP_URL = 'https://freegeoip.app/json/';
-export const IP2_URL = `https://api.ipgeolocation.io/ipgeo?apiKey=${API2_KEY}`;
+export const IP2_URL = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API2_KEY}`;
 
 export const AFTERNOON = '17:00';
 export const NOON = '12:00';
